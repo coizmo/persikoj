f891b7119b87cec174bc92b994faa496869e6c57
coizmo <coizmo@gmail.com>
2021-05-02 01:01:50 +0900

    ✨ prepare to using PrimeVue

--- src/main.js
+++ src/main.js
@@ -2,4 +2,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+// for PrimeVue
+import PrimeVue from 'primevue/config';
+import Button from 'primevue/button';
+
+// for PrimeVue themes
+import 'primevue/resources/primevue.min.css';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primeicons/primeicons.css';
+
+// create vue application
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue);
+
+app.component('Button', Button);
+
+app.mount("#app");
